3cfd1b7a8448a933fc77cde749bb033b8bcf700f
taozhenting@outlook.com <yu82715tzt>
2019-05-09 17:34:52 +0800

    学习

---
+++ python_oldman/三元运算.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+egg_list = []
+for i in range(10):
+    egg_list.append('鸡蛋 %s' %i)
+print(egg_list)
+
+#列表解析法
+l = [ i for i in range(10) ]
+print(l)
+l1 = [ '鸡蛋 %s' %i for i in range(10) ]
+print(l1)
+l2 = [ '鸡蛋 %s' %i for i in range(10) if i > 5 ]
+print(l2)
+#最多三元表达式，没有四元表达式
+# l3 = [ '鸡蛋 %s' %i for i in range(10) if i > 5 else i ]
